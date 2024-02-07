7408d6a3be096efe1f3f2c3316a063770ca88032
Niranjan <140307778+NiranjanAjith@users.noreply.github.com>
2024-02-07 13:38:05 +0530

    Revert "Merge branch 'Stethosense-Codecrafters:master' into master"
    This reverts commit 2218dea20ade7e60e87b6779d5f70b65b1b44fb8, reversing
    changes made to a8bebf1e37754b4179f3d5117764384421eb2993.

--- lab/models.py
+++ lab/models.py
@@ -1,11 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 from autho.models import CustomUser
-
-from django.contrib.auth.models import AbstractUser, Group, Permission
-from django.utils.translation import gettext_lazy as _
-
-
-
 
 # Define choices for the lab_name field
 LAB_REPORT_CHOICES = (
@@ -15,36 +10,19 @@ LAB_REPORT_CHOICES = (
     # Add more choices as needed
 )
 
-
-
-
-class LabUser(AbstractUser):
-    lab_id = models.BigAutoField(primary_key=True)  # Change the ID field name to lab_id
+class LabRegistration(models.Model):
     lab_name = models.CharField(max_length=100)
+    registration_date = models.DateTimeField(auto_now_add=True)
     lab_owner = models.CharField(max_length=100)
     lab_email = models.EmailField(max_length=255, unique=True)
-    lab_password = models.CharField(max_length=128)
+    lab_password = models.CharField(max_length=128)  
     details = models.TextField(blank=True, null=True)
 
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name=_('groups'),
-        blank=True,
-        related_name='lab_users_groups'
-    )
-
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name=_('user permissions'),
-        blank=True,
-        related_name='lab_users_permissions'
-    )
-
-
-
+    def __str__(self):
+        return f"Registration for {self.lab_name} by {self.lab_owner}"
 
 class LabReport(models.Model):
-    lab = models.ForeignKey(LabUser, on_delete=models.CASCADE)
+    lab = models.ForeignKey(LabRegistration, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, choices=LAB_REPORT_CHOICES)
     description = models.TextField()
     date = models.DateField()
--- stethosense/settings.py
+++ stethosense/settings.py
@@ -162,9 +162,3 @@ REST_FRAMEWORK = {
 }
 
 LOGOUT_REDIRECT_URL = '/'
-
-AUTHENTICATION_BACKENDS = [
-    # ...
-    'lab.auth_backends.LabUserBackend',
-    'django.contrib.auth.backends.ModelBackend',
-]
